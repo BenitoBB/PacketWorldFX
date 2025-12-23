8325c078b169915068f7af57ba75c42bacdd979b
gaelmiau <gaelmartinez050@gmail.com>
2025-12-22 18:28:30 -0600

    Clases auxuliares para modulo Cliente

---
+++ src/packetworldfx/interfaz/ISeleccionCliente.java
@@ -0,0 +1,12 @@
+package packetworldfx.interfaz;
+
+import packetworldfx.pojo.Cliente;
+
+/**
+ *
+ * @authors Ohana & Benito
+ */
+public interface ISeleccionCliente {
+    
+    void clienteSeleccionado(Cliente cliente);
+}
